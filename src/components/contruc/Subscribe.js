9499121bdefd0691ac109888ab226578cde0f9f9
Isaiassalas <isaiassalas15.is@gmail.com>
2022-03-31 00:06:13 -0400

    backend

--- src/components/contruc/Subscribe.js
+++ src/components/contruc/Subscribe.js
@@ -1,32 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../styles/Subscribe.css";
 
 const Subscribe = ({
   placeholder,
   placeholder2,
   buttonText,
-  configureNotification,
-  showNotification,
-  changeLogoSpeed
+  
+  
 }) => {
+
+  const [state, setState] = useState({
+    Nombre:"",
+    Correo:""
+    
+  });
+  
+  
+
+  const handleChange = e => {
+     let value = e.target.value;
+     let name = e.target.name;
+
+     setState((prevalue) => {
+      return {
+        ...prevalue,   // Spread Operator               
+        [name]: value
+      }
+    })
+    
+  };
+  const {Nombre,Correo} = state;
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+      const cargar = { Nombre:Nombre,Correo:Correo};
+
+     await fetch("http://localhost/Favorit/api/contact/guardar_datos.php/?insertar", {
+        method:"POST",
+        body: JSON.stringify(cargar) 
+        
+      })
+      
+      setState({ Nombre:"", Correo: "" });
+     
+        alert("Correo enviado correctamente")
+      
+      
+      
+    };
+    
+
+    
+  
 
 
   return (
-    <form className="subscribe" >
+    <form className="subscribe" onSubmit={handleSubmit}>
       
       <input
+       className="subscribe-name"
+       name="Nombre"
+       type="text"
+       placeholder={placeholder2}
+       onChange={handleChange}
+       value={state.Nombre}
+       aria-label="Nombre"
+       required
+     />
+      <input
         className="subscribe-email"
-        name="email"
+        name="Correo"
         type="email"
         placeholder={placeholder}
+        onChange={handleChange}
+        value={state.Correo}
         aria-label="Correo Electronico"
-      />
-       <input
-        className="subscribe-name"
-        name="name"
-        type="text"
-        placeholder={placeholder2}
-        aria-label="Nombre"
+        required
       />
       <button className="subscribe-button" type="submit">
         {buttonText}
